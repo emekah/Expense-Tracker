1cc6c6487a8ea3c72f590575b79d7a0381b76a7b
Emeka Osuji <peacewillemeka@gmail.com>
2021-10-23 22:32:35 +0100

    useState implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Expenses from './components/Expenses/Expenses';
-
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
@@ -27,6 +27,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses  items={expenses}/>
       
     </div>
